707385d554cfc1dca4a70cd55108952eafad03e5
RYAN NICHOLSON <ryan.nicholson23@bathspa.ac.uk>
2026-03-02 16:41:00 +0000

    Version Change Again and Added Notes in Code
    New version 6000.3.7f1

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,10 +5,12 @@ public class PlayerMovement : MonoBehaviour
 {
 
     [SerializeField] private float moveSpeed = 5f;
+
+    // These create variables for components in the player
     private Rigidbody2D rb;
     private Vector2 moveInput;
     private Animator animator;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -25,14 +27,14 @@ public class PlayerMovement : MonoBehaviour
     {
         animator.SetBool("IsWalking", true);
 
-        if (context.canceled)
+        if (context.canceled) //Checks if movement is stopped, then updates Animator variables
         {
-            animator.SetBool("IsWalking", false);
-            animator.SetFloat("LastInputX", moveInput.x);
+            animator.SetBool("IsWalking", false); //Moves animator to idle animations
+            animator.SetFloat("LastInputX", moveInput.x); //These two get direction of movement for correct idle animations
             animator.SetFloat("LastInputY", moveInput.y);
         }
 
-        moveInput = context.ReadValue<Vector2>();
+        moveInput = context.ReadValue<Vector2>(); //Checks movements and updates walking animation for correct animations
         animator.SetFloat("InputX", moveInput.x);
         animator.SetFloat("InputY", moveInput.y);
     }
