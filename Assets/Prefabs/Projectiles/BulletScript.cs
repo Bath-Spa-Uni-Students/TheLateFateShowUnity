112c1e80ae3b8249463b7bdef18d7957fc2c0dbd
Ryan Nicholson <ryan.nicholson23@bathspa.ac.uk>
2026-03-04 20:53:48 +0000

    Enemy Attack and Enemy Detection Radius + Tidied

---
+++ Assets/Prefabs/Projectiles/BulletScript.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float speed;
+
+
+    private Transform player;
+    private Vector2 target;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        target = new Vector2(player.position.x, player.position.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        if(transform.position.x == target.x && transform.position.y == target.y)
+        {
+            DestroyProjectile();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            DestroyProjectile();
+        }
+    }
+
+    void DestroyProjectile()
+    {
+        Destroy(gameObject);
+    }
+}
