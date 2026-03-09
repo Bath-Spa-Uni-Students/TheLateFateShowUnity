488f0325d036fe7ab235441f799844975e62480d
Johnny <johnny.hughes22@bathspa.ac.uk>
2026-03-09 15:47:38 +0000

    Player can shoot out of the point

--- Assets/Player/Weapon.cs
+++ Assets/Player/Weapon.cs
@@ -7,6 +7,12 @@ public class Weapon : MonoBehaviour
     // Store mouse position
     private Vector3 mousePos;
 
+    public GameObject bullet;
+    public Transform bulletTransform;
+    public bool canShoot;
+    private float timer;
+    public float timeBetweenFiring;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,5 +34,22 @@ public class Weapon : MonoBehaviour
 
         // Rotate Z axis
         transform.rotation = Quaternion.Euler(0, 0, rotateZ);
+
+        if(!canShoot)
+        {
+            timer += Time.deltaTime;
+
+            if (timer > timeBetweenFiring)
+            {
+                canShoot = true;
+                timer = 0;
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0) && canShoot)
+        {
+            canShoot = false;
+            Instantiate(bullet, bulletTransform.position, Quaternion.identity);
+        }
     }
 }
