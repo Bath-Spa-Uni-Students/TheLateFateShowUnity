e3ebcbb7943567fa4042d6a45719ee0dc724df3d
Johnny <johnny.hughes22@bathspa.ac.uk>
2026-03-12 01:36:30 +0000

    added comments in PlayerBullet and Weapon scripts

--- Assets/Player/PlayerBullet.cs
+++ Assets/Player/PlayerBullet.cs
@@ -2,31 +2,49 @@ using UnityEngine;
 
 public class PlayerBullet : MonoBehaviour
 {
+    // Reference mouse position
     private Vector3 mousePos;
+
+    // Reference main camera
     private Camera mainCam;
+
+    //Reference rigidbody
     private Rigidbody2D rb;
+
+    // Bullet attributes
     public float bulletSpeed;
     public float bulletDamage;
 
     // Start is called before the first frame update
     private void Start()
     {
+
+        // Gets camera component
         mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+
+        // Gets rigidbody component
         rb = GetComponent<Rigidbody2D>();
+
+        // Gets world coordintes
         mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
 
+        // Bullet travels towards mouse cursor
         Vector3 direction = mousePos - transform.position;
         Vector3 rotation = transform.position - mousePos;
 
+        // Bullet shoots
         rb.linearVelocity = new Vector2 (direction.x, direction.y).normalized * bulletSpeed;
         
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        // Get EnemyHealth script
         EnemyHealth enemy = collision.gameObject.GetComponent<EnemyHealth>();
 
+        // Did the bullet hit an enemy?
         if (collision.gameObject.CompareTag("Enemy"))
         {
+            // Take bullet damage
             enemy.TakeDamage(bulletDamage);
         }
 
--- Assets/Player/Weapon.cs
+++ Assets/Player/Weapon.cs
@@ -2,16 +2,26 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    // Reference main camera
-    private Camera mainCam;
-    // Store mouse position
+    // Reference mouse position
     private Vector3 mousePos;
 
+    // Reference main camera
+    private Camera mainCam;
+
+    // Bullet object
     public GameObject bullet;
+
+    // Where bullet is being shot
     public Transform bulletTransform;
+
+    // Can the player shoot
     public bool canShoot;
+
+    // Internal cooldown
     private float timer;
-    public float timeBetweenFiring;
+
+    // Firerate
+    public float fireRate;
 
     // Start is called before the first frame update
     void Start()
@@ -35,20 +45,27 @@ public class Weapon : MonoBehaviour
         // Rotate Z axis
         transform.rotation = Quaternion.Euler(0, 0, rotateZ);
 
+        // Can the player shoot
         if(!canShoot)
         {
+            // Timer counts up per frame
             timer += Time.deltaTime;
 
-            if (timer > timeBetweenFiring)
+            // Timer greater than firerate?
+            if (timer > fireRate)
             {
+                // Can shoot
                 canShoot = true;
                 timer = 0;
             }
         }
 
+        // Player shoots
         if (Input.GetMouseButtonDown(0) && canShoot)
         {
             canShoot = false;
+
+            // Spawns bullet 
             Instantiate(bullet, bulletTransform.position, Quaternion.identity);
         }
     }
