640bc08a9fc651ebd78d3f695dfef9af47896716
MWright <MOZEZ.WRIGHT22@bathspa.ac.uk>
2026-04-21 14:08:34 +0100

    AR Working (98%) player still holds it backwards
    The ar is in im going to have a look at making the perk manager and centralising the perks system instead of having individual scripts with perks for each gun

--- Assets/Player/Scripts/ARBullet.cs
+++ Assets/Player/Scripts/ARBullet.cs
@@ -1,13 +1,9 @@
 using UnityEngine;
-using static UnityEngine.LowLevelPhysics2D.PhysicsShape;
 
 public class ARBullet : MonoBehaviour
 {
-    public System.Action OnHitEnemy;
-
     // Reference mouse position
     private Vector3 mousePos;
-    private Vector2 direction;
 
     // Reference main camera
     private Camera mainCam;
@@ -15,103 +11,44 @@ public class ARBullet : MonoBehaviour
     //Reference rigidbody
     private Rigidbody2D rb;
 
-    private CircleCollider2D circleCollider;
+    // Bullet attributes
+    public float bulletSpeed;
+    public float bulletDamage;
 
-    [SerializeField] float bulletSpeed = 10;
-    [SerializeField] float bulletDamage = 2;
-
-    [SerializeField] SortingLayer enemyLayer;
-
-    // Call ARPerks script
-    //private ARPerks arPerks;
     public AR ar;
-
-    // How many times bullet bounces for ricochet rounds
-    [SerializeField] int bounces = 2;
 
     // Start is called before the first frame update
     private void Start()
     {
+
         // Gets camera component
         mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
-        circleCollider = GetComponent<CircleCollider2D>();
 
         // Gets rigidbody component
         rb = GetComponent<Rigidbody2D>();
-       // ARPerks = AR.GetComponent<ARPerks>();
 
         // Gets world coordintes
         mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
 
+        // Bullet travels towards mouse cursor
+        Vector3 direction = mousePos - transform.position;
+        Vector3 rotation = transform.position - mousePos;
+
         // Bullet shoots
         // Move in the direction the bullet is facing
         rb.linearVelocity = transform.right * bulletSpeed;
-        direction = transform.right;
 
-        /* If AR perks true
-        if (ARPerks.pierce == true)
-        {
-            // Exclude layers
-            circleCollider.excludeLayers = LayerMask.GetMask("Enemy", "Player", "Player Projectile", "Enemy Projectile");
-        }
     }
-
-    public void Ricochet(Vector2 direction)
-    {
-        this.direction = direction;
-        rb.linearVelocity += direction * bulletSpeed;
-    }
-
-    // Ricochet perk
     private void OnCollisionEnter2D(Collision2D collision)
-    {
-        Debug.Log($"OnCollisionEnter2D hit: {collision.gameObject.name} tag: {collision.gameObject.tag}");
-
-        if (collision.gameObject.CompareTag("Enemy")) return;
-
-
-        if (!ARPerks.ricochet)
-        {
-            Destroy(gameObject);
-            return;
-        }
-
-        bounces--;
-        if (bounces <= 0)
-        {
-            Destroy(gameObject);
-            return;
-        }
-
-        var contact = collision.contacts[0];
-        Vector2 reflected = Vector2.Reflect(direction.normalized, contact.normal);
-        direction = reflected.normalized;
-        rb.linearVelocity = direction * bulletSpeed;
-    }
-
-    // Bullet damage
-    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
             var enemy = collision.gameObject.GetComponent<DamageHandler>();
+
             if (enemy != null)
-            {
-                float damage = bulletDamage;
-                damage = ARPerks.ApplyPowerCell(damage);
-                damage = ARPerks.ApplyCrit(damage);
+                enemy.TakeDamage(bulletDamage); // Damage is applied here
+        }
 
-
-
-                enemy.TakeDamage(damage);
-                ARPerks.HitReloadPerk();
-                ARPerks.LifeStealPerk();
-                ARPerks.ApplyPoisonRounds(enemy);
-                ARPerks.ApplySlowRounds(enemy);
-                ARPerks.ApplyShockwaveLoader(collision.gameObject);
-            }
-
-            if (!ARPerks.pierce) Destroy(gameObject); // <-- add this
-        }*/
+        Destroy(gameObject); // Destroy bullet after hit
     }
 }
