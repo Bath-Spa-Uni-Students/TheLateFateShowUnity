8bcec0701cf22588c0315d7930dc88fa637da404
MOZEZ WRIGHT <mozez.wright22@bathspa.ac.uk>
2026-04-23 17:17:18 +0100

    working perk select screen

--- Assets/PerkSelectionUI.cs
+++ Assets/PerkSelectionUI.cs
@@ -28,3 +28,104 @@ public class PerkSelectionUI : MonoBehaviour
         new PerkOption("Life Steal",        "Heal a portion of damage dealt"),
     };
 
+    private PerkOption[] currentSelection = new PerkOption[3];
+
+    private void Start()
+    {
+        canvas.SetActive(false);
+
+        // Adds listeners to buttons
+        for (int i = 0; i < perkButtons.Length; i++)
+        {
+            int index = i; // capture for lambda
+            perkButtons[i].onClick.AddListener(() => OnPerkSelected(index));
+        }
+    }
+
+    // Show perk screen 
+    public void Show()
+    {
+        currentSelection = GetRandomPerks(3);
+
+        for (int i = 0; i < perkButtons.Length; i++)
+        {
+            perkNameTexts[i].text = currentSelection[i].name;
+            perkDescTexts[i].text = currentSelection[i].description;
+        }
+
+        // Freeze game
+        canvas.SetActive(true);
+        Time.timeScale = 0f;
+
+        AudioManager.Instance.PlayOneShot(FMODEvents.Instance.perkSelect, Vector3.zero);
+    }
+
+    // Hide perk screen
+    private void Hide()
+    {
+        canvas.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    // When a perk is selected apply it and close the screen
+    private void OnPerkSelected(int index)
+    {
+        ApplyPerk(currentSelection[index].name);
+        AudioManager.Instance.PlayOneShot(FMODEvents.Instance.perkTriggerGeneric, Vector3.zero);
+        Hide();
+    }
+
+    // Apply perk to pistol
+    private void ApplyPerk(string perkName)
+    {
+        switch (perkName)
+        {
+            case "Crit Chance": pistolPerks.critChance = true; break;
+            case "Poison Rounds": pistolPerks.poisonRounds = true; break;
+            case "Slow Rounds": pistolPerks.slowRounds = true; break;
+            case "Power Cell": pistolPerks.powerCell = true; break;
+            case "Speed Cell": pistolPerks.speedCell = true; break;
+            case "Shockwave Loader": pistolPerks.shockwaveLoader = true; break;
+            case "Scatter": pistolPerks.scatter = true; break;
+            case "Thorns": pistolPerks.thorns = true; break;
+            case "Pierce": pistolPerks.pierce = true; break;
+            case "Ricochet": pistolPerks.ricochet = true; break;
+            case "Hit Reload": pistolPerks.hitReload = true; break;
+            case "Life Steal": pistolPerks.lifeSteal = true; break;
+        }
+    }
+
+    // Get random perks from the pool
+    private PerkOption[] GetRandomPerks(int count)
+    {
+        PerkOption[] pool = (PerkOption[])allPerks.Clone();
+
+        // Shuffles perk pool
+        for (int i = pool.Length - 1; i > 0; i--)
+        {
+            int j = Random.Range(0, i + 1);
+            PerkOption temp = pool[i];
+            pool[i] = pool[j];
+            pool[j] = temp;
+        }
+
+        PerkOption[] selection = new PerkOption[count];
+        for (int i = 0; i < count; i++)
+            selection[i] = pool[i];
+
+        return selection;
+    }
+}
+
+// Hold perk data
+public class PerkOption
+{
+    public string name;
+    public string description;
+
+    public PerkOption(string name, string description)
+    {
+        this.name = name;
+        this.description = description;
+    }
+}
