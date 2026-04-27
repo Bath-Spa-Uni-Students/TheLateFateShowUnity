a827a4ef174d91104113fc14edec5d3b23d31373
MWright <MOZEZ.WRIGHT22@bathspa.ac.uk>
2026-04-27 12:09:03 +0100

    EnemySpawner Pack Planning
    Decided to go forward with using the prefabs as its simpler thatn harcoding all the values within the script

--- Assets/Enemies/Scripts/EnemySpawner.cs
+++ Assets/Enemies/Scripts/EnemySpawner.cs
@@ -6,11 +6,15 @@ public class EnemySpawner : MonoBehaviour
     [Header("Enemy Prefabs")]
     [SerializeField] private GameObject gruntPrefab;
     [SerializeField] private GameObject speedsterPrefab;
+    [SerializeField] private GameObject gruntFollowerPrefab;
+    [SerializeField] private GameObject gruntLeaderPrefab;
+
 
     [Header("Spawn Points")]
     [SerializeField] private Transform[] spawnPoints;
 
     [Header("Spawn Chances")]
+    [Range(0f, 1f)]
     [SerializeField] private float packSpawnChance = 0.4f;
     [Range(0f, 1f)]
     [SerializeField] private float speedsterSpawnChance = 1f; //set to 1 for testing, will adjust later
@@ -66,6 +70,15 @@ public class EnemySpawner : MonoBehaviour
 
         GameObject enemy = Instantiate(prefab, point.position, Quaternion.identity);// Spawn the enemy at the chosen spawn point
         SpawnManager.Instance.RegisterEnemy();// Register the enemy with the SpawnManager to track the count
+    }
+
+    private void SpawnPack()
+    {
+        //check if at minimum a leader can be spawned
+        //spawn leader grunt
+        //spawn followers based on the current max followers allowed, and the spawn points available and the max cap
+        //ensure followers spawn near the leader and not on top of each other a small radius around the leader
+        //use grunt follower prefab for the followers, and the grunt leader prefab for the leader, or just use the same prefab and have it decide on spawn whether it's a leader or follower based on a bool or something
     }
 
     private Transform GetRandomSpawnPoint()//copilot wrote this for me and it looks good so I kept it, it just picks a random spawn point from the array of spawn points
