6bc229395a744df397f173869bfd3d34804d29df
MWright <MOZEZ.WRIGHT22@bathspa.ac.uk>
2026-04-27 11:48:06 +0100

    Enemy Spawner Planning
    CAnt lie github copilot just wrote half of this but it look right so im goint to try it

--- Assets/Enemies/Scripts/EnemySpawner.cs
+++ Assets/Enemies/Scripts/EnemySpawner.cs
@@ -3,34 +3,59 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    // Get enemy prefabs
-    [SerializeField] private GameObject grunt;
-    //[SerializeField] private GameObject bruiser;
-    [SerializeField] private GameObject speedster;
+    [Header("Enemy Prefabs")]
+    [SerializeField] private GameObject gruntPrefab;
+    [SerializeField] private GameObject speedsterPrefab;
+
+    [Header("Spawn Points")]
+    [SerializeField] private Transform[] spawnPoints;
+
+    [Header("Spawn Chances")]
+    [SerializeField] private float packSpawnChance;
+    [SerializeField] private float speedsterSpawnChance;
+
+    private Coroutine spawnCoroutine;
 
     // Get enemy spawn time
     [SerializeField] private float gruntSpawnTime;
     [SerializeField] private float bruiserSpawnTime;
     [SerializeField] private float speedsterSpawnTime;
 
-    //public GameObject grunt;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-        // Start spawning enemies
-        StartCoroutine(SpawnEnemy(gruntSpawnTime, grunt));
-        //StartCoroutine(SpawnEnemy(bruiserSpawnTime, bruiser));
-        StartCoroutine(SpawnEnemy(speedsterSpawnTime, speedster));
+        spawnCoroutine = StartCoroutine(SpawnLoop());
+
     }
 
-    [SerializeField] private Transform[] spawnPoints;
 
-    private IEnumerator SpawnEnemy(float spawnTime, GameObject enemy)
+    private IEnumerator SpawnLoop()
     {
-        yield return new WaitForSeconds(spawnTime);
-        Transform point = spawnPoints[Random.Range(0, spawnPoints.Length)];
-        Instantiate(enemy, point.position, Quaternion.identity);
-        StartCoroutine(SpawnEnemy(spawnTime, enemy));
+        while (true)
+        {
+           yield return new WaitForSeconds(SpawnManager.Instance.SpawnInterval);
+
+            if (SpawnManager.Instance.CanSpawn)
+                SpawnEnemy();
+            // If cap is hit, loop just waits another interval and tries again
+        }
+    }
+
+    public void SpawnEnemy()
+    {
+        // Determine which enemy to spawn based on chances
+        float roll = Random.value;
+        GameObject enemyToSpawn;
+        if (roll < packSpawnChance)
+            enemyToSpawn = gruntPrefab; // Spawn a pack of grunts
+        else if (roll < packSpawnChance + speedsterSpawnChance)
+            enemyToSpawn = speedsterPrefab; // Spawn a speedster
+        else
+            return; // No spawn this time
+        // Choose a random spawn point
+        Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+        // Instantiate the enemy at the chosen spawn point
+        Instantiate(enemyToSpawn, spawnPoint.position, Quaternion.identity);
     }
 }
