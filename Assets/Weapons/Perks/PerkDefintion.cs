a51c815467afeef90db18c224cdd070636f57cf8
MWright <MOZEZ.WRIGHT22@bathspa.ac.uk>
2026-05-02 09:30:55 +0100

    PerkDefinition
    Created the perk definition script instead of using tags im going to assing the weapons weapontype its just cleaner and more reliable than passing strings

---
+++ Assets/Weapons/Perks/PerkDefintion.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewPerk", menuName = "LateFateShow/Perk Definition")]
+public class PerkDefinition : ScriptableObject
+{
+    [Header("Name")]
+    public string perkName;
+    [TextArea] public string description;
+
+    [Header("Weapon Compatibility")]
+    public bool compatibleWithPistol = true;
+    public bool compatibleWithAR = true;
+    public bool compatibleWithShotgun = true;
+
+    // Returns true if this perk can be applied to the given weapon type.
+    public bool IsCompatibleWith(WeaponType weaponType)
+    {
+        return weaponType switch
+        {
+            WeaponType.Pistol => compatibleWithPistol,
+            WeaponType.AR => compatibleWithAR,
+            WeaponType.Shotgun => compatibleWithShotgun,
+        };
+    }
+}
