eebc00a830c323de83b4d1a408190a40e0376ab9
MWright <MOZEZ.WRIGHT22@bathspa.ac.uk>
2026-05-06 10:40:43 +0100

    Tutorial UI Implementation (Buggy)
    Added some placeholder tutorial ui looks horrendous for now but it works well enough need to hook up the perk and enemy fighting requiremnets

--- Assets/Weapons/TutorialManager.cs
+++ Assets/Weapons/TutorialManager.cs
@@ -1,57 +1,154 @@
 using UnityEngine;
-
+using TMPro;
+using System.Collections;
+using UnityEditor.IMGUI.Controls;
 public class TutorialManager : MonoBehaviour
 {
     public GameObject[] popUps;
     private int popUpIndex = 0;
+    private bool stepCompleted = false;
+
+    [Header("Host")]
+    public Animator hostAnimator;
+    public TMP_Text dialougeText;
+    public GameObject dialougeBox;
+
+    public float typeSpeeed = 0.05f;
+
+    public PlayerMovement playerStats;
+    public SpawnManager spawnManager;
+
+    private Coroutine typeRoutine;
+
+    private static readonly string[] dialougelines = new string[]
+  {
+        "Use WASD to move around.",
+        "Hold Shift to sprint.",
+        "Left Click to shoot.",
+        "Press R to reload.",
+        "You've leveled up! Choose a perk to enhance your weapon.",
+        "Defeat all enemies to progress.",
+        "Good job! Now, let's move on to the maze.",
+  };
+
+    //Animations
+    private static readonly int animTalk = Animator.StringToHash("Talk");
+    private static readonly int animIdle = Animator.StringToHash("Idle");
+    private static readonly int animEcstatic = Animator.StringToHash("Ecstatic");
+
+    private void Start()
+    {
+        ShowStep(0);
+    }
 
     private void Update()
     {
-        for (int i = 0; i < popUps.Length; i++)
-        {
-            if (i == popUpIndex)
-            {
-                popUps[i].SetActive(true);
-            }
-            else
-            {
-                popUps[i].SetActive(false);
-            }
-        }
-        if (popUpIndex == 0)
-        {
-            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
-                popUpIndex++;
-        }
-        else if (popUpIndex == 1)
-        {
-            if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
-                popUpIndex++;
 
-        }
-        else if (popUpIndex == 2)
+        if (stepCompleted) return;
+
+        switch (popUpIndex)
         {
-            if (Input.GetKeyDown(KeyCode.Mouse0))
-                popUpIndex++;
+            case 0://Movement tutorial
+                if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
+                {
+                    AdvanceStep();
+                }
+                break;
+            case 1://Sprint tutorial
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    AdvanceStep();
+                }
+                break;
+            case 2://Shooting tutorial
+                if (Input.GetMouseButtonDown(0))
+                {
+                    AdvanceStep();
+                }
+                break;
+            case 3://Reload tutorial
+                if (Input.GetKeyDown(KeyCode.R))
+                {
+                    AdvanceStep();
+                }
+                break;
+            case 4://Perk selection tutorial
+                //if (//reference perk slots)
+                {
+                    AdvanceStep();
+                }
+                break;
+            case 5://Enemy defeat tutorial
+                // if(spawnManager.AllTutorialEnemiesDefeated())
+                {
+                    AdvanceStep();
+                }
+                break;
+            case 6:
+                TeleportPlayerToMaze();
+                break;
         }
-        else if (popUpIndex == 3)
+    }
+    private void AdvanceStep()
+    {
+       popUpIndex++;
+        if(popUpIndex < popUps.Length)
         {
-            if (Input.GetKeyDown(KeyCode.R))
-                popUpIndex++;
+            ShowStep(popUpIndex);
         }
-        else if (popUpIndex == 4)
+        else
         {
-            //if (player level ==1){
-            popUpIndex++;
+            //end tutorial
+             dialougeBox.SetActive(false);
+             foreach (var popUp in popUps)
+                 popUp.SetActive(false);
         }
-        else if (popUpIndex == 5)
+      
+    }
+
+    private void ShowStep(int index)
+    {
+        for (int i = 0; i < popUps.Length; i++)
+            popUps[i].SetActive(i == index);
+
+        if (typeRoutine != null)
+            StopCoroutine(typeRoutine);
+
+        string line = index < dialougelines.Length ? dialougelines[index] : "No more instructions.";
+
+        typeRoutine = StartCoroutine(TypeWriterRoutine(line, index));
+
+
+    }
+
+    //Anims and Text for host character during dialouge
+    private IEnumerator TypeWriterRoutine(string line,int stepIndex)
+    {
+        stepCompleted = true;
+        dialougeBox.SetActive(true);
+        dialougeText.text = "";
+
+        PlayHostAnimation(stepIndex);
+
+        foreach (char c in line)
         {
-            //if (all enemies defeated){
-            popUpIndex++;
+            dialougeText.text += c;
+            yield return new WaitForSeconds(typeSpeeed);
         }
-        else if (popUpIndex == 6)
-        {
-            //teleport player to maze
-        }
+
+        hostAnimator.CrossFade(animIdle, 0.2f);
+        stepCompleted = false;
+    }
+
+    private void PlayHostAnimation(int stepIndex)
+    {
+      int anim =(stepIndex == 4 || stepIndex == 6) ? animEcstatic : animTalk;
+        hostAnimator.CrossFade(anim, 0.2f);
+    }
+    private void TeleportPlayerToMaze()
+    {
+        // Teleport player to maze entrance
+        playerStats.transform.position = new Vector3(50, 0, 0); // Example position
+        dialougeBox.SetActive(false);
     }
 }
