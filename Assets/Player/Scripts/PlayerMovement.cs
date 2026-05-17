3882119e18b28c41b777cf17cb9e92bd03eb90b8
Ryan Nicholson <ryan.nicholson23@bathspa.ac.uk>
2026-05-17 19:25:57 +0100

    Updated Player Healthbar

--- Assets/Player/Scripts/PlayerMovement.cs
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -1,8 +1,10 @@
+using FMOD.Studio;
+using Microlight.MicroBar;
 using System.Collections;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
-using FMOD.Studio;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -62,7 +64,7 @@ public class PlayerMovement : MonoBehaviour
     private BoxCollider2D boxCollider;
 
     [Tooltip("UI Slider that displays current player health")]
-    [SerializeField] private Slider healthBar;
+    [SerializeField] private MicroBar healthBar;
 
     // ------------------------------------------ //
 
@@ -78,9 +80,10 @@ public class PlayerMovement : MonoBehaviour
 
     void Start()
     {
+        health = maxHealth;
         // Initialise health bar to match starting health value
-        healthBar.maxValue = health;
-        healthBar.value = health;
+        healthBar.Initialize(maxHealth);
+
 
         // Set runtime speed to base walk speed
         moveSpeed = walkSpeed;
@@ -189,7 +192,7 @@ public class PlayerMovement : MonoBehaviour
         if (health <= 0 || health - damage <= 0)
         {
             health -= damage;
-            healthBar.value = health;
+            healthBar.UpdateBar(healthBar.CurrentValue - damage);
             PlayerDie();
         }
         else
@@ -197,7 +200,7 @@ public class PlayerMovement : MonoBehaviour
             playerHurt.start();
             Debug.Log("Player took " + damage + " damage. Remaining health: " + (health - damage));
             health -= damage;
-            healthBar.value = health;
+            healthBar.UpdateBar(healthBar.CurrentValue - damage);
         }
     }
 
